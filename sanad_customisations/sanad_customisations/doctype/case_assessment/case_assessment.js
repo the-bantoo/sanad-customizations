99f5d64c979b6399b9db525c264fb2dd28cf140b
dawoodjee <duncannkhata@gmail.com>
2021-06-23 11:50:07 +0200

    Add Doctype

---
+++ sanad_customisations/sanad_customisations/doctype/case_assessment/case_assessment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Bantoo Accounting Innovations and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Case Assessment', {
+	// refresh: function(frm) {
+
+	// }
+});
